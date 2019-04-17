eff8a9adb7c1e9fce518b5d0b4a82853cfdbcd64
nRaecheR <github.com@bockholdt.info>
2019-04-17 09:51:46 +0200

    [LINUX] Updated to kernel 4.19.35

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -44,8 +44,8 @@ case "$LINUX" in
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     ;;
   *)
-    PKG_VERSION="4.19.34"
-    PKG_SHA256="dd795e2a1fddbee5b03c3bb55a1926829cc08df4fdcabce62dda717ba087b8cc"
+    PKG_VERSION="4.19.35"
+    PKG_SHA256="2fa7e560f0e020104c6e09ffb6c2242a8f5c4b5c39e41ed2996fa7b8b42047e0"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
