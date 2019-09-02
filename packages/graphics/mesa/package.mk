93a0a77fd7b0128cb8d1e729eb4bdbfcddd4dbca
nRaecheR <github.com@bockholdt.info>
2019-09-02 07:28:23 +0200

    MESA: Update to 19.1.4

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="19.1.2"
-PKG_SHA256="08d424da157cd8d00c38584a193aab96873be43d617d1cd78b5b8d33cbcb4ef8"
+PKG_VERSION="19.1.4"
+PKG_SHA256="d4856509db0fb63bfbb89027fc75fed55e5203e1a1be10f768bb6e1992cefc8d"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="https://github.com/mesa3d/mesa/archive/mesa-$PKG_VERSION.tar.gz"
