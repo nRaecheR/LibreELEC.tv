d25e44dfb1c8881505d02c78fe9ea5c0fd99249a
nRaecheR <github.com@bockholdt.info>
2019-11-21 08:18:31 +0100

    [INTEL-UCODE] Updated to 20191115

--- packages/linux-firmware/intel-ucode/package.mk
+++ packages/linux-firmware/intel-ucode/package.mk
@@ -2,11 +2,13 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="intel-ucode"
-PKG_VERSION="20190918"
-PKG_SHA256="2b6b728d351764dfbf6a9763ac96ae7e04085f382a309fed3abc0118f094c943"
+PKG_VERSION="20191115"
+PKG_SHA256="14b26d27be70774948b8cb582e298f5317263b8b8bb1fe8e41260eae54f531dc"
 PKG_ARCH="x86_64"
 PKG_LICENSE="other"
 PKG_SITE="https://downloadcenter.intel.com/search?keyword=linux+microcode"
 PKG_URL="https://github.com/intel/Intel-Linux-Processor-Microcode-Data-Files/archive/microcode-${PKG_VERSION}.tar.gz"
 PKG_LONGDESC="intel-ucode: Intel CPU microcodes"
 PKG_TOOLCHAIN="manual"
+
+
